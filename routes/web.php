c50bba9dc19cd2556a96e9218307000b1edf946e
AhdanFirdaus <muhammadahdanf1@gmail.com>
2026-05-22 13:59:17 +0700

    init: add basic laravel routing

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.beranda');
+});
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::view('/contact','pages.contact');
