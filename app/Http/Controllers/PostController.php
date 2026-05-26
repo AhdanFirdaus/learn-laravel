d3be12847b3764ce3a2a909a1fbb4292031179fb
AhdanFirdaus <muhammadahdanf1@gmail.com>
2026-05-26 11:00:51 +0700

    feat: implement eager loading in api resources

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,4 +20,10 @@ class PostController extends Controller
         $post = Post::with('writer:id,username')->findOrFail($id);
         return new PostDetailResource($post);
     }
+
+    public function show2($id)
+    {
+        $post = Post::findOrFail($id);
+        return new PostDetailResource($post);
+    }
 }
