0145a6bc83abcad01b8372fe54b8012150b6834c
AhdanFirdaus <muhammadahdanf1@gmail.com>
2026-05-26 10:57:13 +0700

    feat: implement api resource collection and relationships

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostDetailResource;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
-use Illuminate\Http\JsonResponse;
+// use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Menampilkan daftar semua post.
-     */
-    public function index(): JsonResponse
-    {
-        // 1. Mengambil semua data dari tabel posts
+    public function index() {
         $posts = Post::all();
+        return PostResource::collection($posts);
+    }
 
-        // 2. Mengembalikan data dalam bentuk JSON dengan HTTP Status 200 (OK)
-        return response()->json(['data'=>$posts], 200);
+    public function show($id)
+    {
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        return new PostDetailResource($post);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -11,4 +12,9 @@ class Post extends Model
         'news_content',
         'author',
     ];
+
+    public function writer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author', 'id');
+    }
 }
