47dceacf415afed3a1149d719caf280f7559e6ca
AhdanFirdaus <muhammadahdanf1@gmail.com>
2026-05-29 15:03:50 +0700

    feat: implement comment CRUD and data relationships

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,13 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
     })
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->alias([
+            'pemilik.komentar' => \App\Http\Middleware\PemilikKomentar::class,
+        ]);
+
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
