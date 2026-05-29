366b22860307d3cd827e52924620924c48db3da5
AhdanFirdaus <muhammadahdanf1@gmail.com>
2026-05-29 11:02:31 +0700

    feat: add create post feature and group middleware implementation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,12 +7,13 @@ use App\Http\Resources\PostResource;
 use App\Models\Post;
 // use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index() {
         $posts = Post::all();
-        return PostResource::collection($posts);
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username'));
     }
 
     public function show($id)
@@ -25,5 +26,17 @@ class PostController extends Controller
     {
         $post = Post::findOrFail($id);
         return new PostDetailResource($post);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required',
+        ]);
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 }
